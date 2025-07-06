711d1b8315b013c35d9d985161e52f792cbd4217
dkerubo <damariskerry@gmail.com>
2025-07-06 22:05:22 +0300

    update files

--- Francilia/francilia/app/payment/page.tsx
+++ Francilia/francilia/app/payment/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+'use client';
 import { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
